074fbb14d2d2a48d7f78143c50e086d30ee7d47e
Anna Ivanova <anna.ivanova@stryker.com>
2023-01-03 12:47:00 +0200

    Added ArrayList for candies

--- src/main/java/org/sweets/Main.java
+++ src/main/java/org/sweets/Main.java
@@ -1,27 +1,34 @@
 package org.sweets;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        Candy candies = new Candy("Snickers", "ChocolateBar", 250);
+        ArrayList<Candy> candiesList = new ArrayList<>();
+        Candy candies0 = new Candy("Snickers", "ChocolateBar", 250);
         Candy candies1 = new Candy("Orbit", "Gum", 50);
         Candy candies2 = new Candy("JellyBeans", "SugarCandy", 150);
 
+        candiesList.add(candies0);
+        candiesList.add(candies1);
+        candiesList.add(candies2);
+
         Present present = new Present("New Year Present", new Candy[]{
-                candies,
+                candies0,
                 candies1,
         });
         Present present2 = new Present("Christmas Present", new Candy[]{
-                candies,
+                candies0,
                 candies2,
 
         });
 
         System.out.println(present.getName() + "\n" + Arrays.toString(present.getCandies()));
+        System.out.println(present2.getName()+ "\n" + candiesList.get(0) + candiesList.get(1));
         System.out.println("The heaviest candy in the present is: " + present.findMaxCandyWeightName());
         System.out.println("The lightest candy in the present is: " + present.findMinCandyWeightName());
         System.out.println("Total present weight is: " + present.getTotalWeight() + " gram");
